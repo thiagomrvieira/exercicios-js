b53036c29b99dcdc541cd585aebb363939544415
Thiago Maciel <thiagomrvieira@gmail.com>
2019-04-17 17:15:44 -0300

    operadores logicos

---
+++ fundamentos/relacionais.js
@@ -0,0 +1,19 @@
+console.log('Teste 01: ', '1' == 1)
+console.log('Teste 02: ', '1' === 1)
+console.log('Teste 03: ', '3' != 3)
+console.log('Teste 04: ', '3' !== 3)
+
+console.log('Teste 05: ', 3 < 2)
+console.log('Teste 06: ', 3 > 2)
+console.log('Teste 07: ', 3 <= 3)
+console.log('Teste 08: ', 3 >= 2)
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+
+console.log('Teste 09: ', d1 == d2)
+console.log('Teste 10: ', d1 === d2)
+
+console.log('Teste 11: ', d1.getTime() === d2.getTime())
+
+ 
