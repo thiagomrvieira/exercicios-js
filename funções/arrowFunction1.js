33f5fa262082edeccd8fc317ab1bb9a625f6abb7
Thiago Maciel <thiagomrvieira@gmail.com>
2019-05-21 15:29:00 -0300

    Funções arrow #02

--- funções/arrowFunction1.js
+++ funções/arrowFunction1.js
@@ -11,8 +11,15 @@ dobro = (a) => {
 dobro = a => 2 * a // return implicito
 console.log(dobro(Math.PI))
 
+
+
+
 let ola = function(){
-    return 'ola'
+    return 'Olá'
 }
+
+//com arrow function
+ola = () => 'Olá'
+
 
 console.log(ola())
