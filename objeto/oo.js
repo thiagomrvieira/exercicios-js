a5d4718cb7ae1a9dd7931c4c0fc42c363370d6e1
Thiago Maciel <thiagomrvieira@gmail.com>
2019-06-07 10:26:04 -0300

    Conceitos de OO

---
+++ objeto/oo.js
@@ -0,0 +1,17 @@
+/*
+Princípios importantes de OO:
+
+1. Abstração: "Traduzir" um objeto do mundo real para o contexto do sistema
+
+2. Encapsulamento: "Esconder" os detalhes de implementação de um objeto fornecendo 
+    uma interface para acesso aos métodos e atributos
+
+3. Herança (prototype): Objeto recebe atributos e comportamentos de um objeto pai
+    Relação "é um": O objeto fiesta é um objeto Carro
+
+4. Polimorfismo: Multiplas formas. À partir de um conceito genérico, substituir por 
+    um objeto concreto.
+
+
+
+*/
