6a8ed0a702c21a0c923c0a4d811c98f935ff1ffe
Thiago Maciel <thiagomrvieira@gmail.com>
2019-06-11 13:36:54 -0300

    JSON vs Objeto

---
+++ objeto/jsonVsObj.js
@@ -0,0 +1,9 @@
+const obj = {a: 1, b: 2, c: 3, soma(){ return a + b + c}}
+console.log(JSON.stringify(obj))
+
+//Formatos inváldos paa coverter texto (json) para Objeto
+//console.log(JSON.parse("{ a:1, b:2, c:3 }"))
+//console.log(JSON.parse("{ 'a':1, 'b':2, 'c':3 }"))
+
+// formato válido
+console.log(JSON.parse('{ "a":1, "b":2, "c":3 }'))
